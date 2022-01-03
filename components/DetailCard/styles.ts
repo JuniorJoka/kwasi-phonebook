9cb956fd69e43dad2b8fbde0a00c94f700b3f70d
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-03 07:53:50 +0000

    add detail card

---
+++ components/DetailCard/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: white;
+  height: 100%;
+  left: 0;
+  overflow-y: scroll;
+  position: fixed;
+  top: 0;
+  width: 100%;
+`;
