b04271b45027233de87d12f5f9258e10ab0c863a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-05 20:11:27 +0000

    add update util func

---
+++ utils/update.ts
@@ -0,0 +1,19 @@
+import { ContactList } from "../shared/types";
+
+export const filter_data = (searchTerm: string, data: ContactList): ContactList => {
+  searchTerm = searchTerm.toLowerCase();
+  const keys = Object.keys(data);
+  const result: ContactList = {};
+  for (let key of keys) {
+    const resultSet = data[key].filter(
+      ({ firstname, lastname, number, email }) =>
+        firstname.toLocaleLowerCase().includes(searchTerm) ||
+        lastname.toLocaleLowerCase().includes(searchTerm) ||
+        number.includes(searchTerm) ||
+        email.includes(searchTerm)
+    );
+    if (resultSet) result[key] = resultSet;
+  }
+
+  return result;
+};
