984caf2f8c64bd19015c77e38fdd2cd52c5f9544
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-05 14:13:18 +0000

    add store

---
+++ store/useStore.ts
@@ -0,0 +1,34 @@
+import create from "zustand";
+import { Store, Contact } from "../shared/types";
+
+const useStore = create<Store>((set) => ({
+  contacts: {},
+  activeContact: null,
+  previousContacts: [],
+  setActive: (contact) => set((state) => ({ activeContact: contact })),
+  addPrevious: (contact) => set((state) => ({ previousContacts: [...state.previousContacts, contact] })),
+
+  addContact: (contact) =>
+    set((state) => {
+      const initial = contact.firstname[0];
+      const categories = state.contacts;
+      if (!categories[initial]) {
+        categories[initial] = [];
+      }
+
+      let contacts = categories[initial];
+      const newContacts = [...contacts, contact];
+      categories[initial] = newContacts;
+
+      return { contacts: categories };
+    }),
+
+  removeContact: (contact: Contact) =>
+    set((state) => {
+      const initial = contact.firstname[0];
+      const categories = state.contacts;
+      let contacts = categories[initial];
+      const newContacts = contacts.filter(({ id }) => contact.id !== id);
+      categories[initial] = newContacts;
+    }),
+}));
