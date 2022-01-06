7ba6dea871c0d2d05acd73a2d35a6a5410d549d4
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-06 17:34:53 +0000

    remove scrollbars from  contactlist

--- components/ContactList/styles.ts
+++ components/ContactList/styles.ts
@@ -1,11 +1,29 @@
 import styled from "styled-components";
+
 import { device } from "../../shared/device";
 
 export const Wrapper = styled.div`
+  color: hsl(200, 50%, 10%);
   @media ${device.tablet} {
-    max-width: 310px;
-    border-right: 10px solid black;
-    overflow-y: scroll;
+    border-right: 10px solid hsl(200, 50%, 10%);
     height: 100vh;
+    overflow-y: scroll;
+    width: 310px;
+
+    ::-webkit-scrollbar {
+      background-color: transparent;
+      height: 3px;
+      width: 0px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: hsl(200, 50%, 10%);
+      border-radius: 6px;
+      height: 4px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: hsla(200, 0%, 0%, 0);
+    }
   }
 `;
