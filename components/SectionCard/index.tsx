37e62049f6371b55deea220d08f19a04978dfda7
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-06 17:52:11 +0000

    add section card

--- components/SectionCard/index.tsx
+++ components/SectionCard/index.tsx
@@ -1,6 +1,11 @@
+import { Contact, SectionProps } from "../../shared/types";
+import useStore from "../../store/useStore";
+import { filter_data } from "../../utils";
 import {
+  Button,
   Card,
   Heading,
+  NoneField,
   ProfileCard,
   ProfileContent,
   ProfileImage,
@@ -9,22 +14,50 @@ import {
   Wrapper,
 } from "./styles";
 
-export const SectionCard = () => {
-  const fakeData = new Array(10).fill(Math.random);
+export const SectionCard = ({ searchTerm, modalHandler, detailHandler }: SectionProps) => {
+  const [data, set] = useStore((state) => [state.contacts, state.setActive]);
+  const contacts = searchTerm ? filter_data(searchTerm, data) : data;
+  const keys = Object.keys(contacts).sort();
+
+  const handler = (contact: Contact) => {
+    set(contact);
+    detailHandler();
+  };
+
   return (
-    <Wrapper>
-      <Heading>A</Heading>
-      <Card>
-        {fakeData.map((key) => (
-          <ProfileCard key={key}>
-            <ProfileImage />
-            <ProfileContent>
-              <ProfileName>Alex Joka</ProfileName>
-              <ProfileNumber>(233) 502190746</ProfileNumber>
-            </ProfileContent>
-          </ProfileCard>
-        ))}
-      </Card>
-    </Wrapper>
+    <>
+      {keys.length ? (
+        keys.map((value) => (
+          <Wrapper key={value}>
+            <Heading>{value}</Heading>
+            <Card>
+              {contacts[value]
+                .sort((a, b) => (a.firstname < b.firstname ? -1 : 1))
+                .map(({ id, firstname, lastname, number, email }) => (
+                  <ProfileCard key={id} onClick={() => handler({ id, firstname, lastname, number, email })}>
+                    <ProfileImage>
+                      {firstname[0].toUpperCase()}
+                      {lastname && lastname[0].toUpperCase()}
+                    </ProfileImage>
+                    <ProfileContent>
+                      <ProfileName>
+                        {firstname} {lastname}
+                      </ProfileName>
+                      <ProfileNumber>{number ? number : "No number"}</ProfileNumber>
+                    </ProfileContent>
+                  </ProfileCard>
+                ))}
+            </Card>
+          </Wrapper>
+        ))
+      ) : (
+        <NoneField>
+          <div>
+            <h2>No Contacts Found</h2>
+            <Button onClick={modalHandler}>Add a Contact</Button>
+          </div>
+        </NoneField>
+      )}
+    </>
   );
 };
--- components/SectionCard/styles.ts
+++ components/SectionCard/styles.ts
@@ -4,39 +4,69 @@ export const Wrapper = styled.div`
   padding: 12px;
 `;
 
-export const Heading = styled.p``;
+export const Heading = styled.p`
+  font-weight: bold;
+  margin: 0 0 12px 4px;
+`;
 
 export const Card = styled.div`
-  background: white;
+  background-color: white;
   border-radius: 8px;
   padding: 12px;
 `;
 
 export const ProfileCard = styled.div`
   align-items: center;
+  cursor: pointer;
   display: flex;
   justify-content: flex-start;
   padding: 8px 0;
 `;
 
 export const ProfileImage = styled.div`
-  background-color: gray;
+  background-color: hsl(200, 50%, 10%);
   border-radius: 8px;
+  color: hsl(200, 20%, 99%);
+  display: grid;
+  font-size: 28px;
+  font-weight: bold;
   height: 48px;
   margin-right: 8px;
+  place-items: center;
   width: 48px;
 `;
 
 export const ProfileContent = styled.div`
-  border-bottom: 0.5px solid gray;
+  border-bottom: 0.5px solid hsl(200, 25%, 90%);
   flex-grow: 1;
   padding: 8px 0;
 `;
 
 export const ProfileName = styled.p`
+  font-weight: bold;
   margin: 0;
 `;
 
 export const ProfileNumber = styled.p`
+  color: hsl(200, 30%, 30%);
   margin: 0;
+`;
+
+export const NoneField = styled.div`
+  display: grid;
+  height: 500px;
+  place-items: center;
+
+  div {
+    display: grid;
+    place-items: center;
+  }
+`;
+
+export const Button = styled.div`
+  background-color: hsl(200, 50%, 10%);
+  border-radius: 8px;
+  color: hsl(200, 20%, 99%);
+  cursor: pointer;
+  padding: 16px;
 `;
