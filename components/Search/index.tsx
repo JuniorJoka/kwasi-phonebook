5cd172bdbfd858d0c8e4ffed28138df19ba51357
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-06 21:13:21 +0000

    add cancel button

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,9 +1,12 @@
+import { XCircle } from "react-feather";
+import { SearchProps } from "../../shared/types";
 import { Wrapper } from "./styles";
 
-export const Search = ({ handler, term }: { handler: (event: any) => void; term: string }) => {
+export const Search = ({ handler, term, resetHandler }: SearchProps) => {
   return (
     <Wrapper>
       <input type="text" name="search" value={term} onChange={handler} />
+      {term && <XCircle onClick={resetHandler} />}
     </Wrapper>
   );
 };
--- components/Search/styles.tsx
+++ components/Search/styles.tsx
@@ -5,18 +5,28 @@ export const Wrapper = styled.div`
   display: grid;
   padding: 12px;
   place-items: center;
+  position: relative;
 
   input {
     background-color: hsl(200, 25%, 90%);
     border-radius: 8px;
     border: none;
     color: hsl(200, 50%, 10%);
+    font-size: 22px;
     height: 36px;
     outline: none;
+    padding: 0 36px 0 8px;
     width: 100%;
 
     &:focus {
       background-color: hsl(200, 20%, 85%);
     }
+  }
+
+  svg {
+    position: absolute;
+    right: 20px;
+    stroke: hsl(200, 50%, 10%);
+    cursor: pointer;
   }
 `;
