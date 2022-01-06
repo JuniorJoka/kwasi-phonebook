83a51164df97061b91fa4670b3bca5b0813b3abd
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-06 17:40:53 +0000

    add detail card comp

--- components/DetailCard/index.tsx
+++ components/DetailCard/index.tsx
@@ -1,5 +1,186 @@
-import { Wrapper } from "./styles";
+import { useTransition } from "@react-spring/web";
+import { useEffect } from "react";
+import { useState } from "react";
+import { CheckCircle, Edit, MessageCircle, Phone, Trash2, Video, X } from "react-feather";
 
-export const DetailCard = () => {
-  return <Wrapper />;
+import { DetailProps, voidFunc } from "../../shared/types";
+import useStore from "../../store/useStore";
+import {
+  Action,
+  Actions,
+  Body,
+  Field,
+  Header,
+  Label,
+  Nav,
+  NoneSelectedField,
+  ProfileHead,
+  ProfileName,
+  Section,
+  Wrapper,
+} from "./styles";
+
+export const DetailCard = ({ onMobile, active, modalHandler, detailHandler }: DetailProps) => {
+  const [editMode, setEditMode] = useState(false);
+
+  const transition = useTransition(active, {
+    from: { y: onMobile ? 500 : 0, opacity: 0 },
+    enter: { y: 0, opacity: 1 },
+    leave: { y: onMobile ? 900 : 0, opacity: 0 },
+  });
+
+  let [contact, edit, set, prev, remove] = useStore((state) => [
+    state.activeContact,
+    state.editContact,
+    state.setActive,
+    state.addPrevious,
+    state.removeContact,
+  ]);
+
+  const [firstname, setFirst] = useState("");
+  const [lastname, setLast] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handler = () => {
+    const currContant = { firstname, lastname, number, email, id: contact?.id || "" };
+    if (firstname) {
+      if (contact) {
+        edit(contact, currContant);
+        set(currContant);
+      }
+    } else {
+      if (contact) {
+        setFirst(contact.firstname);
+      }
+    }
+    setEditMode(false);
+  };
+
+  const deleteHandler = () => {
+    if (contact) {
+      remove(contact);
+      set(null);
+      detailHandler && detailHandler();
+    }
+  };
+
+  const cancelHandler = () => {
+    if (contact) {
+      setFirst(contact.firstname);
+      setLast(contact.lastname);
+      setNumber(contact.number);
+      setEmail(contact.email);
+    }
+    setEditMode(false);
+  };
+
+  useEffect(() => {
+    setEditMode(false);
+    if (contact) {
+      const { firstname, lastname, email, number } = contact;
+      setFirst(firstname);
+      setLast(lastname);
+      setEmail(email);
+      setNumber(number);
+      prev({ ...contact, epoch: Date.now() });
+    }
+  }, [contact]);
+
+  return (
+    <>
+      {transition((style, item) =>
+        item && contact ? (
+          <Wrapper style={style}>
+            <Header>
+              <Nav>
+                {editMode ? <X onClick={cancelHandler} /> : onMobile ? <X onClick={detailHandler} /> : <div />}
+                {editMode ? <CheckCircle onClick={handler} /> : <Edit onClick={() => setEditMode(true)} />}
+              </Nav>
+              <ProfileHead>
+                {contact.firstname[0].toUpperCase()}
+                {contact.lastname && contact.lastname[0].toUpperCase()}
+              </ProfileHead>
+              <ProfileName>
+                {contact.firstname} {contact.lastname}
+              </ProfileName>
+              <Actions>
+                <Action color="hsl(135, 80%, 50%)">
+                  <MessageCircle />
+                </Action>
+                <Action color="hsl(210, 100%, 50%)">
+                  <Phone />
+                </Action>
+                <Action color="hsl(60, 90%, 50%)">
+                  <Video />
+                </Action>
+                <Action color="hsl(340, 100%, 50%)" onClick={deleteHandler}>
+                  <Trash2 />
+                </Action>
+              </Actions>
+            </Header>
+            <Body>
+              <Section>
+                <Label>First name</Label>
+                <Field>
+                  {editMode ? (
+                    <input
+                      type="text"
+                      name="firstname"
+                      value={firstname}
+                      onChange={(e) => setFirst(e.target.value)}
+                      autoFocus
+                    />
+                  ) : (
+                    contact.firstname
+                  )}
+                </Field>
+              </Section>
+              <Section>
+                <Label>Last name</Label>
+                <Field>
+                  {editMode ? (
+                    <input type="text" name="firstname" value={lastname} onChange={(e) => setLast(e.target.value)} />
+                  ) : (
+                    contact.lastname
+                  )}
+                </Field>
+              </Section>
+              <Section>
+                <Label>Phone number</Label>
+                <Field>
+                  {editMode ? (
+                    <input type="number" name="firstname" value={number} onChange={(e) => setNumber(e.target.value)} />
+                  ) : (
+                    contact.number
+                  )}
+                </Field>
+              </Section>
+              <Section>
+                <Label>Email</Label>
+                <Field>
+                  {editMode ? (
+                    <input type="email" name="firstname" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  ) : (
+                    contact.email
+                  )}
+                </Field>
+              </Section>
+            </Body>
+          </Wrapper>
+        ) : (
+          !onMobile && (
+            <NoneSelectedField>
+              <div>
+                <h3>Select a Contact to view Detail</h3>
+                <p>
+                  No Contacts? <span onClick={modalHandler}>Add one</span>
+                </p>
+              </div>
+            </NoneSelectedField>
+          )
+        )
+      )}
+    </>
+  );
 };
--- components/DetailCard/styles.ts
+++ components/DetailCard/styles.ts
@@ -1,11 +1,125 @@
-import styled from "styled-components";
+import { animated } from "@react-spring/web";
 
-export const Wrapper = styled.div`
-  background: white;
-  height: 100%;
-  left: 0;
-  overflow-y: scroll;
+import styled from "styled-components";
+import { device } from "../../shared/device";
+
+export const Wrapper = styled(animated.div)`
+  height: 100vh;
   position: fixed;
   top: 0;
   width: 100%;
+  overflow: scroll;
+  color: hsl(200, 50%, 10%);
+
+  svg {
+    stroke: hsl(200, 50%, 10%);
+    cursor: pointer;
+  }
+
+  @media ${device.tablet} {
+    position: relative;
+    width: inherit;
+  }
+`;
+
+export const Header = styled.div`
+  background-color: hsl(200, 25%, 90%);
+  padding: 20px 20px;
+`;
+
+export const Body = styled.div`
+  background-color: hsl(200, 20%, 99%);
+  height: 80%;
+  padding: 20px;
+`;
+
+export const ProfileHead = styled.div`
+  background-color: hsl(200, 50%, 10%);
+  border-radius: 16px;
+  height: 108px;
+  margin: 0 auto;
+  width: 108px;
+  display: grid;
+  place-items: center;
+  color: hsl(200, 20%, 99%);
+  font-weight: bold;
+  font-size: 36px;
+`;
+
+export const ProfileName = styled.h2`
+  text-align: center;
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 16px auto;
+  max-width: 430px;
+`;
+
+export const Action = styled.div<{ color?: string }>`
+  background-color: hsl(200, 50%, 10%);
+  border-radius: 8px;
+  height: 56px;
+  width: 56px;
+  background-color: ${({ color }) => color};
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+
+  svg {
+    stroke: hsl(200, 20%, 99%);
+  }
+`;
+
+export const Section = styled.div`
+  margin: 18px auto;
+  max-width: 430px;
+
+  input {
+    background-color: hsl(200, 25%, 90%);
+    border: none;
+    font-size: 16px;
+    outline: none;
+    width: 100%;
+    padding: 4px;
+    height: 32px;
+    border-radius: 4px;
+
+    &:focus {
+      background-color: hsl(200, 20%, 85%);
+    }
+  }
+`;
+export const Label = styled.h3`
+  margin: 8px 0;
+`;
+export const Field = styled.div`
+  border-bottom: 2px solid hsl(200, 25%, 90%);
+  height: 32px;
+`;
+
+export const NoneSelectedField = styled.div`
+  display: grid;
+  height: 100%;
+  place-items: center;
+
+  div {
+    display: grid;
+    place-items: center;
+  }
+
+  span {
+    color: blue;
+    cursor: pointer;
+  }
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  margin: 0 auto;
+  margin-bottom: 30px;
+  max-width: 500px;
+  width: 100%;
+  justify-content: space-between;
 `;
