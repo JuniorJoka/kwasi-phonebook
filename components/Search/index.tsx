2dff2254afd49420205cd9472c09dfeae9dd3848
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-06 17:45:58 +0000

    add search component

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,9 +1,9 @@
 import { Wrapper } from "./styles";
 
-export const Search = () => {
+export const Search = ({ handler, term }: { handler: (event: any) => void; term: string }) => {
   return (
     <Wrapper>
-      <input type="text" name="" id="" />
+      <input type="text" name="search" value={term} onChange={handler} />
     </Wrapper>
   );
 };
--- components/Search/styles.tsx
+++ components/Search/styles.tsx
@@ -1,17 +1,22 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background: white;
+  background-color: hsl(200, 20%, 99%);
   display: grid;
   padding: 12px;
   place-items: center;
 
   input {
-    background-color: gray;
+    background-color: hsl(200, 25%, 90%);
     border-radius: 8px;
     border: none;
+    color: hsl(200, 50%, 10%);
     height: 36px;
     outline: none;
     width: 100%;
+
+    &:focus {
+      background-color: hsl(200, 20%, 85%);
+    }
   }
 `;
